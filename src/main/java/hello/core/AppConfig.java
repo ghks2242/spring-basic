70b129ac4ed97c57fd59163b14fca1b6fd68d1be
YCH <ghks629@gmail.com>
2022-09-06 19:14:45 +0900

    study

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    /**
+     * AppConfig는 애플리케이션의 실제동작의 필요한 구현객체를 생성한다.
+     * AppConfig는 생성한객체의 인스턴스를 참조(래퍼런스를) 생성자를 통해 주입해준다.
+     */
+
+    // 생성자를통해서 객체를 주입 => 생성자주입
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+    // 생성자를통해서 객체를 주입 => 생성자주입
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,15 +1,23 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
