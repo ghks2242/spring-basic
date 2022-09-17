c0babc497001238871f7123f3e548e97b5c7ca6a
yunchanghwan <ghks2242@naver.com>
2022-09-17 17:43:41 +0900

    ApplicationContext 설명과 xml 로 설정해보기

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -43,12 +43,21 @@ public class ApplicationContextExtendsFindTest {
     }
 
     @Test
-    @DisplayName("부모 타입으로 조회")
+    @DisplayName("부모 타입으로 모두 조회하기")
     void findAllBeanByParentType() {
         Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
         assertThat(beansOfType.size()).isEqualTo(2);
         for (String key : beansOfType.keySet()) {
             System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - Object")
+    void findALlBeanByObjectType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + "value = " + beansOfType.get(key));
         }
     }
 
