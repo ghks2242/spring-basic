71fa4a874b2921c9f1bc2e664f29e5e3357c37b2
YCH <ghks629@gmail.com>
2022-09-29 18:36:24 +0900

    singleton study

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,51 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+
+        // 과거에 만들었던 스프링 없는 순수한 DI 컨테이너인 AppConfig 는 요청을 할 떄마다 객체를 새로 생성한다.
+        // 고객 트래픽이 초당 100 이 나오면 초당 100 개 객체가 생성되고 소멸된다 -> 메모리 낭비가 심하다.
+        // 해결방안은 해당 객체가 딱 1개가 생성되고, 공유하도록 설계하면된다 -> 싱글톤 패턴.
+
+        AppConfig appConfig = new AppConfig();
+        // 1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+//        assertThat(singletonService1).isEqualTo(singletonService2);
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // same == : 자바의 == 처럼 참조를비교
+        // equal
+    }
+}
